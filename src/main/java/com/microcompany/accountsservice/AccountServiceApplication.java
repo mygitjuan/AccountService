a4bb65f1574f4572b9766e14cd70d3e3c72c6cb7
myGitJuan <jarellano.aguirre@gmail.com>
2024-02-14 11:30:15 +0100

    modificaciones yml

--- src/main/java/com/microcompany/accountsservice/AccountServiceApplication.java
+++ src/main/java/com/microcompany/accountsservice/AccountServiceApplication.java
@@ -1,4 +1,4 @@
-package com.microcompany.accountsservice.AccountService;
+package com.microcompany.accountsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
